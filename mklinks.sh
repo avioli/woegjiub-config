9e56fc8b9813fe4745513e6b495298cb371a268e
woegjiub <woegjiub@gmail.com>
2013-08-06 20:12:08 +1000

    more tidying

--- mklinks.sh
+++ mklinks.sh
@@ -16,3 +16,12 @@ for file in [a-zA-Z]* ; do
 		ln -s `pwd`/"$file" ~/."$file" && echo "Linked ~/.$file to `pwd`/$file" || echo "Couldn't link ~/.$file to `pwd`/$file!"
 	fi
 done
+if [[ -h ~/.bash_profile ]] ; then
+	echo "  Removing symbolic link ~/.bash_profile"
+	rm ~/.bash_profile
+elif [[ -a ~/.bash_profile ]] ; then
+	echo "  ~/.bash_profile exists. Moving it to ~/.bash_profile.bak"
+	mv ~/.bash_profile ~/.bash_profile.bak || echo "Couldn't move ~/.bash_profile to ~/.bash_profile.bak!"
+fi
+ln -s `pwd`/bashrc ~/.bash_profile && echo "Linked ~/bash_profile to `pwd`/bashrc" || echo "Couldn't link ~/.bash_profile to `pwd`/bashrc!"
+
