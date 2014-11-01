14f028665fe3f684dc6fde935e43940caa136839
Chris Darko <chris@ionata.com.au>
2014-11-01 18:50:22 +1100

    closer xdg compliance, only .bash_profile in HOME

--- mklinks.sh
+++ mklinks.sh
@@ -1,43 +1,40 @@
-#!/bin/bash
-cd ./home/
-for file in [a-zA-Z]* ; do
-	if [[ "$file" != "mklinks.sh" &&
-		  "$file" != "README.md" &&
-		  "$file" != "sensitive" ]] ; then
-		if [[ -h ~/."$file" ]] ; then
-			echo "  Removing symbolic link ~/.$file"
-			rm ~/."$file"
-		elif [[ -a ~/."$file" ]] ; then
-			echo "  ~/.$file exists. Moving it to ~/.$file.bak"
-			mv ~/."$file" ~/."$file".bak || echo "Couldn't move ~/.$file to ~/.$file.bak!"
-		fi
-		ln -s `pwd`/"$file" ~/."$file" && echo "Linked ~/.$file to `pwd`/$file" || echo "Couldn't link ~/.$file to `pwd`/$file!"
-	fi
-done
+#!/usr/bin/env bash
+
+if [[ -h ~/.bash_profile ]] ; then
+	echo "  Removing symbolic link ~/.bash_profile"
+	rm ~/.bash_profile
+elif [[ -a ~/.bash_profile ]] ; then
+	echo "  ~/.bash_profile exists. Moving it to ~/.bash_profile.bak"
+	mv ~/.bash_profile ~/.bash_profile.bak ||
+		echo "Couldn't move ~/.bash_profile to ~/.bash_profile.bak!"
+fi
+ln -s `pwd`/bash/bashrc ~/.bash_profile &&
+	echo "Linked ~/.bash_profile to `pwd`/bash/bashrc" ||
+	echo "Couldn't link ~/.bash_profile to `pwd`/bash/bashrc!"
 
 # Create data directories as required
+mkdir -p $XDG_DATA_HOME
 cd $XDG_DATA_HOME
 mkdir -p bash/scripts
-mkdir -p vim/bundle
+mkdir -p less
+mkdir -p vim/{bundle,view}
 
-for var in mozilla pki; do
-	if [[ -d $HOME/.$var && ! -L $HOME/.$var ]]; then
-		echo "  Moving ~/.$var to XDG_DATA_HOME"
-		mv $HOME/.$var $var
-		ln -s `pwd`/$var $HOME/.$var
+for file in mozilla pki; do
+	if [[ -d $HOME/.$file && ! -h $HOME/.$file ]]; then
+		echo "  Moving ~/.$file to XDG_DATA_HOME"
+		mv $HOME/.$file $file
+		ln -s `pwd`/$file $HOME/.$file
 	fi
 done
 
 # Create cache directories as required
+mkdir -p $XDG_CACHE_HOME
 cd $XDG_CACHE_HOME
-mkdir -p bash
-mkdir -p less
-mkdir -p vim/view
 
-for var in adobe cddb dbus macromedia; do
-	if [[ -d $HOME/.$var && ! -L $HOME/.$var ]]; then
-		echo "  Moving ~/.$var to XDG_CACHE_HOME"
-		mv $HOME/.$var $var
-		ln -s `pwd`/$var $HOME/.$var
+for file in adobe cddb dbus macromedia; do
+	if [[ -d $HOME/.$file && ! -h $HOME/.$file ]]; then
+		echo "  Moving ~/.$file to XDG_CACHE_HOME"
+		mv $HOME/.$file $file
+		ln -s `pwd`/$file $HOME/.$file
 	fi
 done
