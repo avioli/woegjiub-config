93f70dd5013a30d9d923addce116515dfb5920f2
Chris Darko <chris@ionata.com.au>
2014-11-02 13:45:09 +1100

    more settings for using buffers instead of tabs

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,7 +13,7 @@ done
 mkdir -p $XDG_DATA_HOME
 cd $XDG_DATA_HOME
 
-for dir in bash/scripts less pip pylint vim/{bundle,view}; do
+for dir in bash/scripts less pip pylint vim/{bundle,view,undo}; do
 	mkdir -p $dir
 	echo "made `pwd`/$dir"
 done
