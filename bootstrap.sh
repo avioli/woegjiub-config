c84bef10dd2c713ebc3b91d56f9ed8a97a1c6e01
Chris Darko <chris@ionata.com.au>
2014-12-24 10:10:21 +1100

    update bootstrapper for recent commits

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,7 +13,9 @@ done
 mkdir -p $XDG_DATA_HOME
 cd $XDG_DATA_HOME
 
-for dir in bash/scripts less pip python pylint vim/{bundle,sessions,view,undo}; do
+for dir in bash/scripts less pip python pylint sqlite3 vim/{bundle,sessions,view,undo}; do
 	mkdir -p $dir
 	echo "made `pwd`/$dir"
 done
+touch sqlite3/sqlite_history
+touch python/python_history
