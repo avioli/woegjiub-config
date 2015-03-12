ff7056960514f1c3d3eacd1521b1e7ffce0cb0d7
Chris Darko <chris@ionata.com.au>
2015-03-12 15:59:06 +1100

    update mkvenv wrapper for src dir differences

--- bash/functions/venv.sh
+++ bash/functions/venv.sh
@@ -61,8 +61,13 @@ function mkvenv() {
 		[[ "$version" -eq 3 ]] && pyvenv "$vdir"
 	fi
 	[[ -d "$path" ]] || mkdir "$path"
-	[[ -e "$path/src/venvlib" ]] || ln -s "$vdir/lib/"*"/site-packages/" "$path/src/venvlib"
-	[[ -e "$path/src/venvsrc" ]] || ln -s "$vdir/src" "$path/src/venvsrc"
+	if [[ -d "$path/src" ]] ; then
+		base="$path/src"
+	elif [[ -d "$path/webapp" ]]; then
+		base="$path/webapp"
+	fi
+	[[ -e "$base/venvlib" ]] || ln -s "$vdir/lib/"*"/site-packages/" "$base/venvlib"
+	[[ -e "$base/venvsrc" ]] || ln -s "$vdir/src" "$base/venvsrc"
 	workon "$name"
 }
 
